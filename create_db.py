c63996253361b0fe7d68fcff0ae0f70c06ca70f5
Exactra <malikhamdan070@gmail.com>
2024-12-24 11:51:43 +0500

    employee.py and all other files

---
+++ create_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+def create_db():
+    con=sqlite3.connect(database=r'ims.db')
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT,name text,cnic text,gender text,contact text,dob text,doj text,pass text,utype text,address text,salary text)")
+    con.commit()
+    
+    
+    
+create_db()
